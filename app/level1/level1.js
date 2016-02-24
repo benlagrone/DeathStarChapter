2fba967f002842713a906081f177910304a175d0
BenLaGrone <benjaminlagrone@gmail.com>
2016-02-24 08:44:23 -0600

    moving parallax to levels general patterns

--- app/level1/level1.js
+++ app/level1/level1.js
@@ -28,75 +28,87 @@ level1.parseAjax = function (xhr,id){
     level1StarsHTML += '</div>';
     document.getElementById('p1').innerHTML=level1StarsHTML;
 
-
-};
-
-
-level1.spreadObjects = function(x,vm,hm,va,ha,p,e){
-    for (var i=0;i<x.length;i++){
-        x[i].style.position = p;
-        x[i].style.top = Math.floor((Math.random()*vm)+va)+e;
-        x[i].style.left = Math.floor((Math.random()*hm)+ha)+e;
+    for (var key in level1.data.objectgroups.clouds){
+        var cloudHTMLStart = '<div class="clouds">';
+        for(var key0 in level1.data.objectgroups.clouds[key].objects){
+            cloudHTMLStart+='<i class="'+level1.data.objectgroups.clouds[key].objects[key0].idclass+' '+level1.data.objectgroups.clouds[key].objects[key0].sizeclass+' '+level1.data.objectgroups.clouds[key].objects[key0].colorclass+'"></i>';
+        }
+        cloudHTMLStart+= '</div>';
+        document.getElementById(key).innerHTML=cloudHTMLStart;
     }
+
+    var objectsHTMLStart = '';
+    for (var key in level1.data.objectgroups.objects){
+        objectsHTMLStart+='<div id="'+key+'">';
+        if(key==='rocket')
+            objectsHTMLStart+='<span>';
+        for(var key0 in level1.data.objectgroups.objects[key].objects){
+            objectsHTMLStart+='<i class="'+level1.data.objectgroups.objects[key].objects[key0].idclass+' '+level1.data.objectgroups.objects[key].objects[key0].sizeclass+' '+level1.data.objectgroups.objects[key].objects[key0].colorclass+'"></i>';
+        }
+        if(key==='rocket')
+            objectsHTMLStart+='</span>';
+        objectsHTMLStart+='</div>';
+    }
+    document.getElementById('objects').innerHTML=objectsHTMLStart;
+
+    var terraHTMLStart = '<div id="ground">';
+    for (var key in level1.data.objectgroups.terra){
+        terraHTMLStart+='<div class="'+key+'">';
+        for(var key0 in level1.data.objectgroups.terra[key].objects){
+            terraHTMLStart+='<i class="'+level1.data.objectgroups.terra[key].objects[key0].idclass+' '+level1.data.objectgroups.terra[key].objects[key0].sizeclass+' '+level1.data.objectgroups.terra[key].objects[key0].colorclass+'"></i>';
+        }
+        terraHTMLStart+='</div>';
+    }
+    terraHTMLStart+='</div>';
+    document.getElementById('terra').innerHTML=terraHTMLStart;
+
+    levels.spreadObjects(document.getElementById("stars").getElementsByTagName("i"),150,100,1,1,"fixed","%");
+    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("fa-tree"),0,14,-(window.innerHeight/28),1,"relative","px");
+    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("right")[0].getElementsByClassName("small"),0,14,-(window.innerHeight/13),1,"relative","px");
+    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("right")[0].getElementsByClassName("large"),0,14,-(window.innerHeight/15),1,"relative","px");
+    for (var i = 0; i < document.getElementsByClassName("clouds").length; i++){
+        levels.spreadObjects(document.getElementsByClassName("clouds")[i].getElementsByTagName("i"),window.innerHeight*.75,window.innerWidth*.75,1,1-(window.innerWidth/2),"relative","px");
+    }
+
 };
+
 
 level1.updateElement = function() {
+
     level1.getMovingElements(function (theObject,increment){
         theObject.style.position = "relative";
-        theObject.style.left = level1.setElementLeftPosition(theObject,increment);
+        theObject.style.left = levels.setElementLeftPosition(theObject,increment);
     });
-    level1.moveEarth(document.getElementById("earth"));
-    level1.moveRocket(document.getElementById("rocket"));
+    levels.moveEarth(document.getElementById("earth"));
+    levels.moveRocket(document.getElementById("rocket"));
     for (var i =0; i < document.getElementById("stars").getElementsByTagName("i").length; i++){
         document.getElementById("stars").getElementsByTagName("i")[i].style.opacity = (1-(window.pageYOffset/(window.innerHeight*(document.getElementsByClassName("row").length))) -.3);
     }
-}
+};
 
-level1.moveRocket = function(rocket){
-    rocket.getElementsByTagName("span")[0].style.transform = "rotate(" + (355 - (((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight))*3) + "deg)";
-    rocket.getElementsByTagName("i")[2].style.transform = "rotate(" + (259 - (((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight))*3) + "deg)";
-    rocket.getElementsByTagName("span")[0].style.bottom = 65 * (document.getElementById("rocket").getBoundingClientRect().bottom)/(window.innerHeight*(document.getElementsByClassName("row").length)) + '%';
-}
-
-level1.moveEarth = function(earth){
-    earth.style.fontSize=(((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight) *100)+"vw";
-    earth.style.left=(((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight) + document.getElementsByClassName("row").length*2)+"px";
-    earth.style.height = (window.innerHeight*(document.getElementsByClassName("row").length)-window.pageYOffset)/window.innerHeight+.5 + '%';
-    earth.getElementsByTagName("i")[0].style.transform = "rotate(" + (15 + (((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight))*4) + "deg)";
-    earth.getElementsByTagName("i")[0].style.right = (window.pageYOffset/window.innerHeight*45) + '%';
-    earth.getElementsByTagName("i")[0].style.bottom = (window.pageYOffset/window.innerHeight*200) + '%';
-    earth.getElementsByTagName("i")[0].style.opacity = 1.3-(window.pageYOffset/(window.innerHeight*(document.getElementsByClassName("row").length)));
-}
 
 level1.cloudCall = function(clouds){
     for (var k=0;k<clouds.classList.length;k++){
         switch (clouds.classList[k]){
             case 'fa-2x':
-                clouds.style.left = level1.setElementLeftPosition(clouds,1);
+                clouds.style.left = levels.setElementLeftPosition(clouds,1);
                 break;
             case 'fa-3x':
-                clouds.style.left = level1.setElementLeftPosition(clouds,2);
+                clouds.style.left = levels.setElementLeftPosition(clouds,2);
                 break;
             case 'fa-4x':
-                clouds.style.left = level1.setElementLeftPosition(clouds,3);
+                clouds.style.left = levels.setElementLeftPosition(clouds,3);
                 break;
             case 'fa-5x':
-                clouds.style.left = level1.setElementLeftPosition(clouds,4);
+                clouds.style.left = levels.setElementLeftPosition(clouds,4);
                 break;
             default:
-                clouds.style.left = level1.setElementLeftPosition(clouds,.5);
+                clouds.style.left = levels.setElementLeftPosition(clouds,.5);
                 ;
         }
     }
-}
+};
 
-level1.setElementLeftPosition = function(element,increment){
-    if(isNaN(parseInt(element.style.left.split("p")[0]))){
-        return ((element.getBoundingClientRect().left)+increment)+"px"
-    } else {
-        return ((Math.abs(parseInt(element.style.left.split("p")[0]))) + increment)+"px";
-    }
-}
 
 level1.getMovingElements = function(callback){
     for(var i = 0;i<document.getElementsByClassName("row").length;i++){
@@ -123,38 +135,9 @@ level1.getMovingElements = function(callback){
             }
         }
     }
-}
+};
 
-window.smoothScrollTo = (function () {
-    var timer, start, factor;
 
-    return function (target, duration) {
-        var offset = window.pageYOffset,
-            delta  = target - window.pageYOffset; // Y-offset difference
-        duration = duration || 1000;              // default 1 sec animation
-        start = Date.now();                       // get start time
-        factor = 0;
-
-        if( timer ) {
-            clearInterval(timer); // stop any running animations
-        }
-
-        function step() {
-            var y;
-            factor = (Date.now() - start) / duration; // get interpolation factor
-            if( factor >= 1 ) {
-                clearInterval(timer); // stop animation
-                factor = 1;           // clip to max 1.0
-            }
-            y = factor * delta + offset;
-            window.scrollBy(0, y - window.pageYOffset);
-        }
-
-        timer = setInterval(step,10);
-
-        return timer;
-    };
-}());
 
 
 if(id==='home'){
@@ -162,4 +145,4 @@ if(id==='home'){
 }else{
     levels.load('level1.updateElement()');
     services.getPage(pageRoute.data,'level1',level1.parseAjax,id);
-};
+}
--- app/levels.js
+++ app/levels.js
@@ -6,3 +6,69 @@ levels.load = function(levelCallBack){
     smoothScrollTo(document.body.scrollHeight);
     document.getElementsByTagName("body")[0].setAttribute("onscroll",levelCallBack);
 };
+
+
+levels.spreadObjects = function(x,vm,hm,va,ha,p,e){
+    for (var i=0;i<x.length;i++){
+        x[i].style.position = p;
+        x[i].style.top = Math.floor((Math.random()*vm)+va)+e;
+        x[i].style.left = Math.floor((Math.random()*hm)+ha)+e;
+    }
+};
+
+
+levels.moveRocket = function(rocket){
+    rocket.getElementsByTagName("span")[0].style.transform = "rotate(" + (355 - (((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight))*3) + "deg)";
+    rocket.getElementsByTagName("i")[2].style.transform = "rotate(" + (259 - (((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight))*3) + "deg)";
+    rocket.getElementsByTagName("span")[0].style.bottom = 65 * (document.getElementById("rocket").getBoundingClientRect().bottom)/(window.innerHeight*(document.getElementsByClassName("row").length)) + '%';
+};
+
+levels.moveEarth = function(earth){
+    earth.style.fontSize=(((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight) *100)+"vw";
+    earth.style.left=(((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight) + document.getElementsByClassName("row").length*2)+"px";
+    earth.style.height = (window.innerHeight*(document.getElementsByClassName("row").length)-window.pageYOffset)/window.innerHeight+.5 + '%';
+    earth.getElementsByTagName("i")[0].style.transform = "rotate(" + (15 + (((window.innerHeight*(document.getElementsByClassName("row").length) - document.getElementById("rocket").getBoundingClientRect().bottom)/window.innerHeight))*4) + "deg)";
+    earth.getElementsByTagName("i")[0].style.right = (window.pageYOffset/window.innerHeight*45) + '%';
+    earth.getElementsByTagName("i")[0].style.bottom = (window.pageYOffset/window.innerHeight*200) + '%';
+    earth.getElementsByTagName("i")[0].style.opacity = 1.3-(window.pageYOffset/(window.innerHeight*(document.getElementsByClassName("row").length)));
+};
+
+
+levels.setElementLeftPosition = function(element,increment){
+    if(isNaN(parseInt(element.style.left.split("p")[0]))){
+        return ((element.getBoundingClientRect().left)+increment)+"px"
+    } else {
+        return ((Math.abs(parseInt(element.style.left.split("p")[0]))) + increment)+"px";
+    }
+};
+
+window.smoothScrollTo = (function () {
+    var timer, start, factor;
+
+    return function (target, duration) {
+        var offset = window.pageYOffset,
+            delta  = target - window.pageYOffset; // Y-offset difference
+        duration = duration || 1000;              // default 1 sec animation
+        start = Date.now();                       // get start time
+        factor = 0;
+
+        if( timer ) {
+            clearInterval(timer); // stop any running animations
+        }
+
+        function step() {
+            var y;
+            factor = (Date.now() - start) / duration; // get interpolation factor
+            if( factor >= 1 ) {
+                clearInterval(timer); // stop animation
+                factor = 1;           // clip to max 1.0
+            }
+            y = factor * delta + offset;
+            window.scrollBy(0, y - window.pageYOffset);
+        }
+
+        timer = setInterval(step,10);
+
+        return timer;
+    };
+}());
